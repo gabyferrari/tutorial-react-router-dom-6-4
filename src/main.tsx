f5dc6dc2c78ececc2e26860d45d08a32fb88a170
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-06-13 17:12:36 -0300

    Index routes

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,17 @@ import Expenses from "./routes/Expenses/index.tsx";
 import Invoices from "./routes/Invoices/index.tsx";
 import NotFound from "./routes/NotFound/index.tsx";
 import Invoice from "./routes/Invoices/Invoice/index.tsx";
+import InvoicesIndex from "./routes/Invoices/InvoicesIndex/index.tsx";
+import AppIndex from "./routes/AppIndex/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
+        <Route index element={<AppIndex />} />
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />}>
+          <Route index element={<InvoicesIndex />} />
           <Route path=":invoiceId" element={<Invoice />} />
         </Route>
         <Route path="*" element={<NotFound />}
---
+++ src/routes/AppIndex/index.tsx
@@ -0,0 +1,8 @@
+export default function AppIndex() {
+    
+  return (
+    <main style={{ padding: "1rem" }}>
+      <p>Welcome!</p>
+    </main>
+  );
+}
